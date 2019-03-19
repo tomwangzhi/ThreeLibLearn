a4678104750de14b514e90071a30062d57172030
wangzhi <wang.zhi@intellif.com>
2019-03-19 21:15:42 +0800

    构建简单maven项目，来测试apache common-beanutils包下面的一些方法

---
+++ CommonUtilJarLearn/ApacheJar/src/main/java/commonBeanUtils/MethodOperateUtil.java
@@ -0,0 +1,12 @@
+package commonBeanUtils;
+
+/**
+ * use MethodUtils to write some demo in order to better understanding
+ */
+public class MethodOperateUtil {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ CommonUtilJarLearn/ApacheJar/src/main/java/commonBeanUtils/User.java
@@ -0,0 +1,44 @@
+package commonBeanUtils;
+
+/**
+ * Test common-beanutils bean
+ */
+public class User {
+
+    private String name;
+    private String password;
+    private String email;
+    private String birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+}
